fde4adf9589f3603bd2d63f251b3217add1223c2
Oleg <pakolegdev@gmail.com>
2023-03-22 00:19:51 +0500

    Code refactoring

--- app/src/main/java/com/example/garbagecollector/util/Constants.kt
+++ app/src/main/java/com/example/garbagecollector/util/Constants.kt
@@ -4,7 +4,8 @@ class Constants {
     companion object {
         const val DATABASE_NAME = "GarbageCollector"
         const val REQUEST_LOCATION = 1
-        const val HOME_TAG = "Home Fragment"
+        const val HOME_FRAGMENT_TAG = "Home Fragment"
+        const val HOME_VIEW_MODEL_TAG = "HomeViewModel"
         const val GARBAGE_IMAGE_INTENT = "data"
         const val REQUEST_CAMERA = 100
     }
--- app/src/main/java/com/example/garbagecollector/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/garbagecollector/viewmodel/HomeViewModel.kt
@@ -2,24 +2,50 @@ package com.example.garbagecollector.viewmodel
 
 import android.app.Application
 import android.graphics.Bitmap
+import android.location.Address
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.garbagecollector.model.Location
 import com.example.garbagecollector.repository.LocationRepository
-import com.google.android.gms.maps.model.Marker
+import com.example.garbagecollector.util.Constants
+import com.google.android.gms.maps.model.LatLng
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val TAG = "MapsViewModel"
     private val locationRepository: LocationRepository = LocationRepository(getApplication())
+    private var locations: LiveData<List<Location>>? = null
 
-    fun addLocation(marker: Marker, image: Bitmap?) {
-        val bookmark = locationRepository.createLocation()
-        bookmark.placeId = marker.id
-        bookmark.name = marker.title.toString()
-        bookmark.longitude = marker.position.longitude
-        bookmark.latitude = marker.position.latitude
+    fun addLocation(latLng: LatLng, address: Address, garbagePhoto: Bitmap) {
+        val location = locationRepository.createLocation()
+        location.name = address.featureName
+        location.city = address.locality
+        location.postalCode = address.postalCode.toInt()
+        location.longitude = latLng.longitude
+        location.latitude = latLng.latitude
+        location.photo = garbagePhoto
 
-        val newId = locationRepository.addLocation(bookmark)
-        Log.i(TAG, "New bookmark $newId added to the database.")
+        val newId = locationRepository.addLocation(location)
+        Log.i(Constants.HOME_VIEW_MODEL_TAG, "New location $newId added to the database.")
     }
+
+
+    fun getMarkers(): LiveData<List<Location>>? {
+        if (locations == null) {
+            locations = locationRepository.allLocations
+        }
+        return locations
+    }
+
+//    private fun mapLocationsToMarkerOptions() {
+//        //Get Location Objects from the Repository and convert them to MarkerOptions
+//        locations = locationRepository.allLocations.map { repoBookmarks ->
+//            repoBookmarks.map { bookmark -> locationToMarker(bookmark) }
+//        }
+//    }
+//
+//    private fun locationToMarker(location: Location): MarkerOptions {
+//        val latLng = LatLng(location.latitude, location.longitude)
+//        return MarkerOptions().position(latLng)
+//    }
 }
