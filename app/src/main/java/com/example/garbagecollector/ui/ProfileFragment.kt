e126b33ef80b0985a4d60bc9123d4d814a15dc90
Oleg <pakolegdev@gmail.com>
2023-03-23 23:42:32 +0500

    finish Profile screen view

--- app/src/main/java/com/example/garbagecollector/ui/ProfileFragment.kt
+++ app/src/main/java/com/example/garbagecollector/ui/ProfileFragment.kt
@@ -1,25 +1,60 @@
 package com.example.garbagecollector.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.viewModels
 import com.example.garbagecollector.R
+import com.example.garbagecollector.databinding.ProfileBinding
 import com.example.garbagecollector.viewmodel.ProfileViewModel
 
 class ProfileFragment : Fragment() {
+    private val profileViewModel by viewModels<ProfileViewModel>()
 
-    companion object {
-        fun newInstance() = ProfileFragment()
-    }
+    private lateinit var binding: ProfileBinding
 
-    private lateinit var viewModel: ProfileViewModel
+    private val commonListNames = listOf("My Garbage", "Ranking", "Change Language", "Notifications")
+    private val additionalListNames = listOf("FAQ", "About App")
+
+    private var commonListViewAdapter: ArrayAdapter<String>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.profile, container, false)
+        binding = ProfileBinding.inflate(inflater)
+
+        val commonListView = binding.commonListView
+        val additionalListView = binding.additionalListView
+        commonListViewAdapter =
+            activity?.let { ArrayAdapter(it, R.layout.list_view_item, commonListNames) }
+
+        val additionalListViewAdapter =
+            activity?.let { ArrayAdapter(it, R.layout.list_view_item, additionalListNames) }
+
+        commonListView.adapter = commonListViewAdapter
+        additionalListView.adapter = additionalListViewAdapter
+
+        commonListView.setOnItemClickListener { parent, view, position, id ->
+            setCommonLitItemListener(position)
+        }
+
+        return binding.root
+    }
+
+    private fun setCommonLitItemListener(listItemPosition: Int) {
+        when (commonListViewAdapter?.getItem(listItemPosition).toString()) {
+            "My Garbage" -> {
+                val intent = Intent(requireActivity(), MyGarbageActivity::class.java)
+                startActivity(intent)
+            }
+            "Ranking" -> {
+
+            }
+        }
     }
 }
