e83188e0d306cd1ad979bc64d0c1e97009a8ba8b
Oleg <pakolegdev@gmail.com>
2023-04-08 15:53:40 +0500

    add hyperlinks in SignIn/Up pages

--- app/src/main/java/com/example/garbagecollector/ui/LoginActivity.kt
+++ app/src/main/java/com/example/garbagecollector/ui/LoginActivity.kt
@@ -3,14 +3,21 @@ package com.example.garbagecollector.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.activity.viewModels
+import com.example.garbagecollector.R
 import com.example.garbagecollector.api.dto.LoginDto
 import com.example.garbagecollector.databinding.LoginBinding
 import com.example.garbagecollector.viewmodel.ProfileViewModel
 
 class LoginActivity : AppCompatActivity() {
-    private val viewModels by viewModels<ProfileViewModel>()
     private lateinit var binding: LoginBinding
+    private val viewModels by viewModels<ProfileViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,10 +32,25 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        setSignUpLink()
     }
 
     private fun login(email: String, password: String) {
         val loginDto = LoginDto(email, password)
         viewModels.loginUser(loginDto)
+    }
+
+    private fun setSignUpLink() {
+        val text = getString(R.string.sign_up_title)
+        val spannableString = SpannableString(text)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@LoginActivity, RegistrationActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        spannableString.setSpan(clickableSpan, 23, 30, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.signUpLink.setText(spannableString, TextView.BufferType.SPANNABLE)
+        binding.signUpLink.movementMethod = LinkMovementMethod.getInstance()
     }
 }
--- app/src/main/java/com/example/garbagecollector/ui/RegistrationActivity.kt
+++ app/src/main/java/com/example/garbagecollector/ui/RegistrationActivity.kt
@@ -2,6 +2,12 @@ package com.example.garbagecollector.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.example.garbagecollector.api.dto.RegistrationDto
@@ -32,6 +38,17 @@ class RegistrationActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+        val text = "Already Registered? Sign In"
+        val spannableString = SpannableString(text)
+        val clickableSpan = object : ClickableSpan() {
+            override fun onClick(widget: View) {
+                val intent = Intent(this@RegistrationActivity, LoginActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        spannableString.setSpan(clickableSpan, 20, 27, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        binding.signIn.setText(spannableString, TextView.BufferType.SPANNABLE)
+        binding.signIn.movementMethod = LinkMovementMethod.getInstance()
     }
 
     private fun registerUser(firstName: String, lastName: String, login: String, password: String) {
