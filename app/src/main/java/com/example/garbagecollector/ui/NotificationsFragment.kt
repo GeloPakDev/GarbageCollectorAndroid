abdd43b664a5888686ce0ea4d077a423731ef2e2
Oleg <pakolegdev@gmail.com>
2023-04-11 17:20:04 +0500

    code refactoring

--- app/src/main/java/com/example/garbagecollector/ui/NotificationsFragment.kt
+++ app/src/main/java/com/example/garbagecollector/ui/NotificationsFragment.kt
@@ -5,23 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.garbagecollector.R
-import com.example.garbagecollector.viewmodel.NotificationsViewModel
+import com.example.garbagecollector.databinding.NotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = NotificationsFragment()
-    }
+    private lateinit var binding: NotificationsBinding
 
-    private lateinit var viewModel: NotificationsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.notifications, container, false)
+        binding = NotificationsBinding.inflate(inflater)
+        return binding.root
     }
-
-
 }
