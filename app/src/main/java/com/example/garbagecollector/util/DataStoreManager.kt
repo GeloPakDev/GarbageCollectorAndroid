27d0d57ac155c38e2f8bbf2e0779122eee14aba2
Oleg <pakolegdev@gmail.com>
2023-04-11 10:48:52 +0500

    add userId property to store it in the DataStore

--- app/src/main/java/com/example/garbagecollector/util/DataStoreManager.kt
+++ app/src/main/java/com/example/garbagecollector/util/DataStoreManager.kt
@@ -2,9 +2,7 @@ package com.example.garbagecollector.util
 
 import android.content.Context
 import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.core.edit
-import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.core.*
 import androidx.datastore.preferences.preferencesDataStore
 import com.example.garbagecollector.api.dto.LoginJWTDto
 import kotlinx.coroutines.flow.Flow
@@ -14,8 +12,9 @@ class DataStoreManager(private val context: Context) {
     companion object {
         val AUTH_TOKEN = stringPreferencesKey("JWT_TOKEN")
         val USER_EMAIL = stringPreferencesKey("USER_EMAIL")
+        val USER_ID = longPreferencesKey("USER_ID")
         private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
-            name = "AUTHORIZATION"
+            name = Constants.DATASTORE_NAME
         )
     }
 
@@ -26,6 +25,16 @@ class DataStoreManager(private val context: Context) {
 
     val userEmail: Flow<String> = context.dataStore.data.map { preferences ->
         preferences[USER_EMAIL] ?: ""
+    }
+
+    val userId: Flow<Long> = context.dataStore.data.map { preferences ->
+        preferences[USER_ID] ?: 0L
+    }
+
+    suspend fun saveUserId(userId: Long) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_ID] = userId
+        }
     }
 
     suspend fun saveUserCredentials(loginJWTDto: LoginJWTDto) {
